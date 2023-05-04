8489d345d449d856c4517ec56d8ab205f42b2394
Дабек Хаупшев <dabek643@gmail.com>
2023-05-04 11:43:51 +0300

    Review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -55,8 +55,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 class FollowViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
-    viewsets.GenericViewSet
-    ):
+    viewsets.GenericViewSet):
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
     filter_backends = (filters.SearchFilter,)
